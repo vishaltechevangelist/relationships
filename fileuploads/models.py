6e3bd94ef0304e47300563dbdeff9851c8598a9f
vishaltechevangelist <37928313+vishaltechevangelist@users.noreply.github.com>
2025-04-23 23:05:52 +0530

    updating upload type to Image

--- fileuploads/models.py
+++ fileuploads/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 class UserData(models.Model):
     username = models.CharField(max_length=255)
-    file = models.FileField(upload_to='userdata/')
+    # file = models.FileField(upload_to='userdata/')
+    file = models.ImageField(upload_to='userdata/')
